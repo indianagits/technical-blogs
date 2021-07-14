be6fbdd006ffde5aa7fd4518efb56816784827c9
Rahul Yadav <rahulyadav7477@gmail.com>
2021-07-14 21:13:47 +0530

    BinarySearchTree in java

---
+++ src/search/BinarySearchTree.java
@@ -0,0 +1,120 @@
+package search;
+
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int key) {
+        this.key = key;
+    }
+}
+
+class BST {
+
+    private Node root;
+
+    public void insert(int key) {
+        root = insert(root, key);
+    }
+
+    private Node insert(Node node, int key) {
+        if (node == null) {
+            return new Node(key);
+        }
+        if (key <= node.key) {
+            node.left = insert(node.left, key);
+        } else {
+            node.right = insert(node.right, key);
+        }
+        return node;
+    }
+
+    public Node search(int key) {
+        return search(root, key);
+    }
+
+    private Node search(Node node, int key) {
+        if (node == null || node.key == key) {
+            return node;
+        }
+        if (key < node.key) {
+            return search(node.left, key);
+        }
+        return search(node.right, key);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+        System.out.println();
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.key + " ");
+        inOrder(node.right);
+    }
+
+    public void preOrder() {
+        preOrder(root);
+        System.out.println();
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.key + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+        System.out.println();
+    }
+
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.key + " ");
+    }
+}
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.insert(10);
+        bst.insert(20);
+        bst.insert(5);
+        bst.insert(7);
+        bst.insert(18);
+        bst.insert(25);
+
+        System.out.println("The inOrder traversal is: ");
+        bst.inOrder();
+
+        System.out.println("The preOrder traversal is: ");
+        bst.preOrder();
+
+        System.out.println("The postOrder traversal is: ");
+        bst.postOrder();
+
+        search(bst, 18);
+        search(bst, 22);
+    }
+
+    private static void search(BST bst, int key) {
+        if (bst.search(key) != null) {
+            System.out.println(key + " found");
+        } else {
+            System.out.println(key + " not found");
+        }
+    }
+}
